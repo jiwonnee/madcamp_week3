e1f45fc8b06b10255e9a6fa42bef483cf4193add
sinjiwon <shinjw0619@gmail.com>
2024-07-13 13:57:48 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Header from './components/Header';
+import HomePage from './components/HomePage';
+import Mypage from './pages/Mypage';
+import Gonggu from './pages/Gonggu';
+import Wishlist from './pages/Wishlist';
+import SignIn from './pages/SignIn';
+import Register from './pages/Register'; // Register 컴포넌트 import
 import './App.css';
 
 function App() {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header isHomePage={isHomePage} />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/gonggu" element={<Gonggu />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/register" element={<Register />} /> {/* Register 페이지 라우트 추가 */}
+      </Routes>
     </div>
   );
 }
